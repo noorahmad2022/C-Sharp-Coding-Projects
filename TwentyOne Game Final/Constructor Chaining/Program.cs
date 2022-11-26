49ccb7649c3a217abcc469e690d94b7373659afb
Noorahmad Noor <102923335+noorahmad2022@users.noreply.github.com>
2022-11-26 14:36:30 -0800

    Additional Features Assignment

---
+++ TwentyOne Game Final/Constructor Chaining/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Chaining
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var newEmployee = new constChain("Noorahmad Noor");  // creating a variable using var and passing just Name. the default ID '100' will assign
+            constChain newEmployee1 = new constChain(200,"Jesse"); // createing another veriable, passing the ID and name.
+
+
+            Console.WriteLine("Employee ID: "+newEmployee.id +"\n"+"Employee Name: "+ newEmployee.Name +"\n");
+            Console.WriteLine("Employee ID: " + newEmployee1.id + "\n" + "Employee Name: " + newEmployee1.Name);
+
+
+
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ TwentyOne Game Final/Constructor Chaining/constChain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Chaining
+{
+    public class constChain
+    {
+        // calling of contructor from another constructor
+
+
+        
+        public int id;
+        public string Name;
+    
+        public constChain(string Name):this(100,Name)   // this constructor take name from name, if not assign the ID it will assign 100.
+        {
+
+        }
+
+        public constChain(int id, string Name)
+        {
+            this.Name = Name;
+            this.id = id;   
+        }
+
+    }
+}
