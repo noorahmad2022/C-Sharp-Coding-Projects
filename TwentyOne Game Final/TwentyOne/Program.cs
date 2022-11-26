e8f7399a9077616015d837c7d0d1c782f0d0ed82
Noorahmad Noor <102923335+noorahmad2022@users.noreply.github.com>
2022-11-26 10:25:42 -0800

    Final Game

---
+++ TwentyOne Game Final/TwentyOne/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name. ");
+            string PlayerName = Console.ReadLine();
+
+            Console.WriteLine("And how much money did you bring today?");
+            int bank = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now? (Yes/No)", PlayerName);
+            string answer = Console.ReadLine().ToLower();
+
+            if (answer == "Yes" || answer == "yes" || answer == "Yeah" || answer == "y")
+            {
+                Player player = new Player(PlayerName, bank);
+                Game game = new TwentyOneGame();
+                game += player;
+                player.isActivePlaying = true;
+
+                while(player.isActivePlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Thank you for playing!");
+            }
+
+            Console.WriteLine("Feel free to look around the casio. Bye for now.");
+            Console.Read();
+        }
+    }
+}
